c82db671a25b60399a67f3ef542afeddc5e4b99a
Mitchell Morris <mgm@Mitchell-PC.(none)>
2009-02-19 20:52:38 -0800

    Initial import into Curse.com's repository

---
+++ MakeRoom_enUS.lua
@@ -0,0 +1,7 @@
+local AL3 = LibStub("AceLocale-3.0")
+local T = AL3:NewLocale("MakeRoom", "enUS", true, true)
+
+T["INVENTORY_IS_FULL"] = "Inventory is full."
+T["DIRECTIONS"] = "shift-click to destroy"
+T["CURSOR_BUSY"] = "You're already holding something"
+T["NO_GREY_ITEMS"] = "No grey items to destroy."

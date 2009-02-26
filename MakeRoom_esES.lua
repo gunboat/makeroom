d473e693c6d4d6a0c71ee8223070d2d794be76cb
Mitchell Morris <mgm@Mitchell-PC.(none)>
2009-02-25 20:24:00 -0800

    Fix locale string for esES, handle case where locale object can't be created

--- MakeRoom_esES.lua
+++ MakeRoom_esES.lua
@@ -1,9 +1,11 @@
 local AL3 = LibStub("AceLocale-3.0")
-local L = AL3:NewLocale("MakeRoom", "esMX", false)
+local L = AL3:NewLocale("MakeRoom", "esES", false)
 
-L["CURSOR_BUSY"] = "Ya estas sosteniendo algo"
-L["DESTROY_ALL"] = "Destruir todos"
-L["INVENTORY_IS_FULL"] = "El inventario esta lleno."
-L["MAKEROOM_WINDOW_TITLE"] = "MakeRoom"
-L["NO_GREY_ITEMS"] = "No hay objetos grices para destruir."
-L["TOOLTIP_INSTRUCTIONS"] = "Shift-Click para destruir."
+if L then
+    L["CURSOR_BUSY"] = "Ya estas sosteniendo algo"
+    L["DESTROY_ALL"] = "Destruir todos"
+    L["INVENTORY_IS_FULL"] = "El inventario esta lleno."
+    L["MAKEROOM_WINDOW_TITLE"] = "MakeRoom"
+    L["NO_GREY_ITEMS"] = "No hay objetos grices para destruir."
+    L["TOOLTIP_INSTRUCTIONS"] = "Shift-Click para destruir."
+end
